6bfed2f77045d91a0eb03ea43c9a6419f7a8e2b4
Jharif Nelson Aparicio Casillas <jharif.aparicio.casillas@usfx.bo>
2024-09-05 18:39:12 -0400

    se elimino el codigo de libros innesesario y se inicio el codigo de usuarios

---
+++ controllers/usuarioController.js
@@ -0,0 +1,64 @@
+const UsuarioService = require('../services/usuarioService');
+
+class UsuarioController {
+    async createUsuario(req,res){
+        try {
+            const {nombre,correo,contraseña} = req.body;
+            const id = await UsuarioService.createUsuario(nombre,correo,contraseña);
+            res.status(201).json({id});
+        } catch(err) {
+            res.status(500).json({message: err.message});
+        }
+    }
+
+    async getUsuarios(req,res){
+        try {
+            const usuarios = await UsuarioService.getUsuarios();
+            res.json(usuarios);
+        } catch(err) {
+            res.status(400).json({message: err.message});
+        }
+    }
+
+    async getUsuarioById(req,res){
+        try {
+            const {id} = req.params;
+            const usuario = await UsuarioService.getUsuarioById(id);
+            if(!usuario){
+                res.status(404).json({message: 'Usuario no encontrado'});
+            }
+            res.json(usuario);
+        } catch(error) {
+            res.status(500).json({error: error.message});
+        }
+    }
+
+    async updateUsuario(req,res){
+        try {
+            const {id} = req.params;
+            const {nombre,correo,contraseña} = req.body;
+            const changes = await UsuarioService.updateUsuario(id,nombre,correo,contraseña);
+            if(changes == 0){
+                res.status(404).json({message: 'Usuario no encontrado'});
+            }
+            res.json({message: 'Usuario actualizado'});
+        } catch(err) {
+            res.status(500).json({error: err.message});
+        }
+    }
+
+    async deleteUsuario(req,res){
+        try {
+            const {id} = req.params;
+            const changes = await UsuarioService.deleteUsuario(id);
+            if(changes == 0){
+                res.status(404).json({message: 'Usuario no encontrado'});
+            }
+            res.json({message: 'Usuario eliminado'});
+        } catch(err) {
+            res.status(500).json({error: err.message});
+        }
+    }
+}
+
+module.exports = new UsuarioController();
--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 require ('dotenv').config();
 const express = require('express');
-
 const app = express();
 const port = process.env.PORT || 3000;
 
 //importing routes
-const bookRoutes = require('./routes/bookRoutes');
+const usuarioRoutes = require('./routes/usuarioRoutes');
 
 //middleware
 app.use(express.json());
 
 //routes
-app.use('/api', bookRoutes);
+app.use('/usuarios', usuarioRoutes);
 
 //initialize server
 app.listen(port, () => {
