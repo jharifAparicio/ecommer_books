a7b8f6777cd0d0bf4d5a490bec839758b8e3a71f
Jharif Nelson Aparicio Casillas <jharif.aparicio.casillas@usfx.bo>
2024-09-05 15:42:30 -0400

    chore: Update .env file and package-lock.json

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
+
 const BookController = require('../controllers/BookController');
+
 const router = express.Router();
 
 router.get('/books', BookController.getAllBooks);
