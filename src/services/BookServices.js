413e323b662f39c7279863afc59aedf8b34e0a9f
Jharif Nelson Aparicio Casillas <jharif.aparicio.casillas@usfx.bo>
2024-09-22 09:24:23 -0400

    Agregar clase BookRepository a BookServices.js

---
+++ src/services/BookServices.js
@@ -0,0 +1,22 @@
+const BookRepository = require('../repositories/BookRepository');
+
+class  BookRepository{
+    static async createBook(title,author,isbn,editorial,price,stock,dateCreation,link_imagen,CategoryId){
+        const book = {title,author,isbn,editorial,price,stock,dateCreation,link_imagen,CategoryId};
+        return BookRepository.createBook(book);
+    }
+    static async getBookByTitle(title){
+        return BookRepository.getBookByTitle(title);
+    }
+    static async getAllBooks(){
+        return BookRepository.getAllBooks();
+    }
+    static async updateBookStock(title,stock){
+        return BookRepository.updateBookStock(title,stock);
+    }
+    static async deleteBook(title){
+        return BookRepository.deleteBook(title);
+    }
+}
+
+module.exports = BookRepository;
