cbc08c25f5b9975fe9adfca5cf8603ee7600ecd3
Jharif Nelson Aparicio Casillas <jharif.aparicio.casillas@usfx.bo>
2024-09-26 20:22:19 -0400

    Agregar columna de stock a la tabla de libros

--- src/repositories/BookRepository.js
+++ src/repositories/BookRepository.js
@@ -4,9 +4,9 @@ const BookModel = require('../models/BookModel');
 class BookRepository {
     static async createBook(book) {
         const { id, title, author,first_publish_year,cover_image } = book;
-        const sql = 'INSERT INTO books (id, title, author,first_publish_year,cover_image) VALUES (?,?,?,?,?)';
+        const sql = 'INSERT INTO books (id, title, author,first_publish_year,cover_image,stock) VALUES (?,?,?,?,?)';
         return new Promise((resolve, reject) => {
-            BookDB.run(sql, [id, title, author,first_publish_year,cover_image], function(err) {
+            BookDB.run(sql, [id, title, author,first_publish_year,cover_image,stock], function(err) {
                 if (err) {
                     return reject(err);
                 }
@@ -38,29 +38,27 @@ class BookRepository {
                 if (err) {
                     return reject(err);
                 }
-                const books = rows.map(row => new BookModel(row.id,row.title,row.author,row.first_publish_year,row.cover_image));
+                const books = rows.map(row => new BookModel(row.id,row.title,row.author,row.first_publish_year,row.cover_image,row.stock));
                 resolve(books);
             });
         });
     }
 
-    static async updateBookStock(title, stock) {
-        const sql = 'UPDATE books SET stock = ? WHERE title = ?';
+    static async updateBook(id, updatedBook) {
+        const { title, author, first_publish_year, cover_image } = updatedBook;
+        const sql = 'UPDATE books SET title = ?, author = ?, first_publish_year = ?, cover_image = ? WHERE id = ?';
         return new Promise((resolve, reject) => {
-            BookDB.run(sql, [stock, title], function(err) {
+            BookDB.run(sql, [title, author, first_publish_year, cover_image, id], function(err) {
                 if (err) {
                     return reject(err);
                 }
-                if (this.changes === 0) {
-                    return reject({message: 'Libro no encontrado', status: 404});
-                }
-                resolve({message: `Stock actualizado ${title}`});
+                resolve({ message: `Libro actualizado con éxito`, updatedBook });
             });
         });
-    }
+    }    
 
     static async deleteBook(title) {
-        const sql = 'DELETE FROM books WHERE title = ?';
+        const sql = 'DELETE FROM books WHERE id = ?';
         return new Promise((resolve, reject) => {
             BookDB.run(sql, [title], function(err) {
                 if (err) {
